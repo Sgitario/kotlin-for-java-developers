7780f52415edca55a70c0917ae25fae98106c9a7
Jose Carvajal <jose.carvajal@piksel.com>
2018-10-23 16:10:25 +0200

    Init commit

---
+++ src/examples/java/Strings.java
@@ -0,0 +1,8 @@
+package examples.java;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Jose";
+        System.out.println(String.format("Hello %s!", name));
+    }
+}
